ae27d12a5bded4ad30bd747d283eea701ace7652
prs-dev <pratyushsri.25@gmail.com>
2025-08-08 14:27:27 +0530

    debouncing done, core done, ai remains

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,13 +1,15 @@
 import NotesCard from "./NotesCard"
 import { useStore } from "../store/notesStore"
+import {useDebounce} from '../hooks/useDebounce'
 
 const NotesList = () => {
   const notes = useStore(state => state.notes)
   const searchTerm = useStore(state => state.searchTerm)
+  const debounced = useDebounce(searchTerm, 500)
   console.log(notes)
   return (
     <div className="flex gap-10 items-center justify-center flex-wrap py-4">
-      {notes.filter(i => i.content.includes(searchTerm)).map(item => <NotesCard note={item} />)}
+      {notes.filter(i => i.content.includes(debounced)).map(item => <NotesCard note={item} />)}
     </div>
   )
 }
